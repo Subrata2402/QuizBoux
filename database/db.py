f9a8ebd2144b9330257084726d614aae1e36c46b
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2022-01-26 21:43:15 +0530

    Create db.py

---
+++ database/db.py
@@ -0,0 +1,5 @@
+from pymongo import MongoClient
+
+data = MongoClient('mongodb+srv://Subrata2001:Subrata2001@cluster0.ywnwn.mongodb.net/MimirQuiz?retryWrites=true&w=majority')
+db = data.get_database("MimirQuiz")
+question_base = db.questions
