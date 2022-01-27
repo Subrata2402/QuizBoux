a0b60488e3fc48d3ded9a97c534c4f6c44011e79
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2022-01-27 07:15:47 +0530

    update ws.py

--- Websocket/ws.py
+++ Websocket/ws.py
@@ -33,7 +33,6 @@ class Websocket:
 	async def get_token(self):
 		token = db.token.find_one({"id": "3250"})["token"]
 		self.token = token
-		print(self.token)
 
 	async def send_hook(self, content = "", embed = None):
 		async with aiohttp.ClientSession() as session:
