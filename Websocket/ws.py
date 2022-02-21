a223e0344213fdd5929ac3af307a4bb926478703
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2022-02-21 22:33:52 +0530

    update ws.py

--- Websocket/ws.py
+++ Websocket/ws.py
@@ -208,7 +208,7 @@ class Websocket:
 		}
 		self.headers = headers
 		async with aiohttp.ClientSession() as session:
-			async with session.get(url = url) as response:
+			async with session.get(url = url, headers = headers) as response:
 				if response.status != 200:
 					await self.send_hook("**The Token has Expired!**")
 					raise commands.CommandError("Token has expired!")
