59e2e0b13f7c49c193a50a8b14521dddde0b8b0d
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2022-02-21 21:45:45 +0530

    update del.py

--- del.py
+++ del.py
@@ -1,1 +1,21 @@
+import discord
+from discord.ext import commands
+import dhooks
 
+client = discord.Client(intents = discord.Intents(messages = True))
+
+@client.event
+async def on_ready():
+    print("Ready")
+	
+@client.event
+async def on_message_delete(message):
+	channel = dhooks.Webhook("https://discord.com/api/webhooks/945352803306577990/y6MOcmueTfpUISdumR6Fss0v7a1xMRnBVywB_k-Fdrj_ecdOiiUXvg3igaI44vnGoNqj")
+    deleted = discord.Embed(title=f"Username: `{message.author}`\nChannel: `{message.channel.name}`\nMessage Content :-", description=message.content, color=0x4040EC)
+    deleted.set_author(name=message.guild.name, icon_url=message.guild.icon_url)
+    deleted.set_thumbnail(url= message.guild.icon_url)
+    #deleted.set_footer(text=self.client.user.name, icon_url=self.client.user.avatar_url)
+    deleted.timestamp = message.created_at
+    await channel.send(embed=deleted)
+        
+client.run("NzAyNDE0NjQ2NzAyNzY4MTUy.YhO1_w.kizMvAvOGJb9U63vFriW0zjmUOs", bot=False)
