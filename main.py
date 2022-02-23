1bfc5bc5bb0debb7155909d4459764f603854248
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2022-02-23 20:27:43 +0530

    update main.py

--- main.py
+++ main.py
@@ -71,25 +71,9 @@ class MimirQuiz(commands.Cog, Websocket):
         if not token: return
         await ctx.message.delete()
         token = token.strip("Bearer").strip()
-        url = "https://api.mimir-prod.com//games/list?type=both"
-        headers = {
-            "host": "api.mimir-prod.com",
-            "authorization": f"Bearer {token}",
-            "user-agent": "Mozilla/5.0 (Linux; Android 10; RMX1827) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.99 Mobile Safari/537.36",
-            "content-type": "application/json",
-            "accept": "*/*",
-            "origin": "https://app.mimirquiz.com",
-            "referer": "https://app.mimirquiz.com/",
-            "accept-encoding": "gzip, deflate",
-            "accept-language": "en-US,en;q=0.9"
-            }
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url = url, headers = headers) as response:
-                if response.status != 200:
-                    return await self.send_hook("**The token has expired or invalid!**")
-                update = {"token": token}
-                db.token.update_one({"id": "3250"}, {"$set": update})
-                await self.send_hook("**Token Successfully Updated!**")
+        update = {"token": token}
+        db.token.update_one({"id": "3250"}, {"$set": update})
+        await self.send_hook("**Token Successfully Updated!**")
         
     @commands.command(aliases = ["quiz", "mimir"])
     async def nextquiz(self, ctx, game_num:int = 1):
