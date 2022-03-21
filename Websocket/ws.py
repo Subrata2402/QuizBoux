0be8bab8fb0bb6223c7172f01128c49ebfbb27da
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2022-03-21 10:08:01 +0530

    update ws.py

--- Websocket/ws.py
+++ Websocket/ws.py
@@ -13,7 +13,7 @@ google_question = "https://google.com/search?q="
 question_number = total_question = 0
 from database import db
 order = ["１", "２", "３", "４", "５", "６", "７", "８", "９", "０"]
-ignore_options = ["the", "a", "of", "in", "&",
+ignore_options = ["the", "of", "in", "&", "on", "for", "or",
 "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
 ]
 replace_options = {"1": "one", "2": "two", "3": "three", "4": "four", "5": "five",
