9fac5cb62636f1dbe100158ee138ee6b31a6b2e7
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2022-04-05 21:18:22 +0530

    update db.py

--- database/db.py
+++ database/db.py
@@ -2,4 +2,4 @@ from pymongo import MongoClient
 
 data = MongoClient('mongodb+srv://Subrata2001:Subrata2001@cluster0.ywnwn.mongodb.net/MimirQuiz?retryWrites=true&w=majority')
 db = data.get_database("MimirQuiz")
-token = db.guild_token
+mimir_details = db.mimir_details
