0872667ea9c417348408800842f922c51179614d
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2022-04-07 09:18:26 +0530

    update ws.py

--- Websocket/ws.py
+++ Websocket/ws.py
@@ -341,6 +341,7 @@ class Websocket:
 		except:
 			return await self.send_hook("**Failed to Connect Websocket!**")
 		self.ws_is_opened = True
+		print(messages)
 		for msg in messages:
 			event = msg.event
 			print(event)
@@ -423,6 +424,31 @@ class Websocket:
 					#Google Search Results 2
 					try:
 						await self.rating_search_two(google_question, choices, 1)
+					except Exception as e:
+						print(e)
+						
+					try:
+						count_options = {}
+						for choice in choices:
+							option = unidecode(choice["choice"]).strip()
+							_option = replace_options.get(option)
+							option = _option if _option else option
+							count_option = self.searching_data.count(option.lower())
+							count_options[option] = count_option
+						max_count = max(list(count_options.values()))
+						min_count = min(list(count_options.values()))
+						#min_max_count = min_count if not_question else max_count
+						embed = discord.Embed(title=f"**__Search Results -{order[index]}__**", color = discord.Colour.random())
+						embed.set_footer(text = "Mimir Quiz")
+						embed.timestamp = datetime.datetime.utcnow()
+						description = ""
+						for index, option in enumerate(count_options):
+							if max_count != 0 and count_options[option] == max_count:
+								description += f"{order[index]}. {option} : {count_options[option]} ✅\n"
+							else:
+								description += f"{order[index]}. {option} : {count_options[option]}\n"
+						embed.description = f"**{description}**"
+						await self.send_hook(embed = embed)
 					except Exception as e:
 						print(e)
 					
