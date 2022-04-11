85719c599e8a297531c4fdd10f1bc3def6fdf28b
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2022-04-11 23:20:17 +0530

    update main.py

--- main.py
+++ main.py
@@ -60,6 +60,29 @@ class MimirQuiz(commands.Cog, Websocket):
         token = db.mimir_details.find_one({"guild_id": guild_id}).get("token")
         await ctx.send(f"```\n{token}\n```")
     
+    @commands.command(aliases = ["p"])
+    async def price(self, ctx, mimir:float = None):
+        """Get or calculate current price of Mimir Token."""
+        ws = Websocket(ctx.guild.id)
+        web_url = await ws.get_web_url()
+        if not web_url: return await ctx.reply(ctx.author.mention + ", Channel not setup for Mimir Quiz.")
+        url = "https://api.coingecko.com/api/v3/coins/mimir-token"
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url = url) as response:
+                if response.status != 200:
+                    return await ws.send_hook("**Something unexpected happened while fetching current price!**")
+                if not mimir: mimir = 1.0
+                data = await response.json()
+                name = data.get("name")
+                price = data.get("market_data").get("current_price").get("usd")
+                usd = float("{:.2f}".format(price*mimir))
+                price = data.get("market_data").get("current_price").get("inr")
+                inr = float("{:.2f}".format(price*mimir))
+                embed = discord.Embed(
+                    color = discord.Colour.random(),
+                    title = f"**__Current Price of {name}__**",
+                    description = f"**ᛗ{mimir} ≈ ${usd} ≈ ₹{inr}**")
+                await ws.send_hook(embed = embed)
 
     @commands.command()
     async def addtoken(self, ctx, *, token = None):
