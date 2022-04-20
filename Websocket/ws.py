980b9615afe2cbd443babbdf679006a47c335a07
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2022-04-20 22:59:24 +0530

    update ws.py

--- Websocket/ws.py
+++ Websocket/ws.py
@@ -12,9 +12,18 @@ from database import db
 order = ["１", "２", "３", "４", "５", "６", "７", "８", "９", "０"]
 ignore_options = ["the", "of", "in", "&", "on", "for", "or", "it", "to", "at", "and", "?", "#", "!",
 "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+"name", "does", "do", "is", "am", "are", "have", "has", "been", "did", "was", "not", "least", "never", "don't", "haven't", "didn't", "wasn't", "except", "wouldn't", "itsn't",
+"were", "had", "will", "would", "shall", "can", "should", "could", "may", "might", "need", "come", "comes", "means",
+"what", "who", "which", "whom", "why", "how", "when", "where",
+"of", "on", "these", "that", "this", "those", "there", "their", "at", "between", "from", "since", "for",
+"they", "and", "the", "a", "an", "with", "as", "by", "in", "to", "into", "also", "but",
+"i", "my", "me", "we", "our", "you", "your", "he", "his", "him", "himself", "them", "themselves", "it", "its", "myself", "she", "her", "yourselves",
+"&", ".", "?", ",", "matched", "paired", "pair"
 ]
 replace_options = {"1": "one", "2": "two", "3": "three", "4": "four", "5": "five",
 					"6": "six", "7": "seven", "8": "eight", "9": "nine", "10": "ten"}
+negative_words = {" not ", " least ", " never ", " incorrect ", " incorrectly ", " none ", " cannot ", " can't ", " didn't "}
+
 
 class Websocket(object):
 	
