108cec228d91caa687ae079b98990ed2dd024365
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2022-04-20 23:05:54 +0530

    update ws.py

--- Websocket/ws.py
+++ Websocket/ws.py
@@ -14,11 +14,11 @@ ignore_options = ["the", "of", "in", "&", "on", "for", "or", "it", "to", "at", "
 "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
 "name", "does", "do", "is", "am", "are", "have", "has", "been", "did", "was", "not", "least", "never", "don't", "haven't", "didn't", "wasn't", "except", "wouldn't", "itsn't",
 "were", "had", "will", "would", "shall", "can", "should", "could", "may", "might", "need", "come", "comes", "means",
-"what", "who", "which", "whom", "why", "how", "when", "where",
+"what", "who", "which", "whom", "why", "how", "when", "where", "=",
 "of", "on", "these", "that", "this", "those", "there", "their", "at", "between", "from", "since", "for",
 "they", "and", "the", "a", "an", "with", "as", "by", "in", "to", "into", "also", "but",
 "i", "my", "me", "we", "our", "you", "your", "he", "his", "him", "himself", "them", "themselves", "it", "its", "myself", "she", "her", "yourselves",
-"&", ".", "?", ",", "matched", "paired", "pair"
+"&", ".", "?", ",", "matched", "paired", "pair", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"
 ]
 replace_options = {"1": "one", "2": "two", "3": "three", "4": "four", "5": "five",
 					"6": "six", "7": "seven", "8": "eight", "9": "nine", "10": "ten"}
