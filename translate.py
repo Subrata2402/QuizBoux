30a39e1949ff23ba4704f0922ee20d068ff2813e
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2022-05-01 13:09:43 +0530

    update translate.py

--- translate.py
+++ translate.py
@@ -1,1 +1,8 @@
-translated = GoogleTranslator(source='auto', target='de').translate("keep it up, you are awesome")
+from deep_translator import GoogleTranslator
+from database import db
+
+async def translate(guild_id, text):
+	language = db.mimir_details.find_one({"guild_id": guild_id}).get("language")
+	if not language: language = "en"
+	translated = GoogleTranslator(source='auto', target=language).translate(text)
+	return translated
