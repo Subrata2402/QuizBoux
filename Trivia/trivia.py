e60bdaeb3bdc090749f8e2378d4718d3f813f40d
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2022-05-06 19:05:41 +0530

    update trivia.py

--- Trivia/trivia.py
+++ Trivia/trivia.py
@@ -1,6 +1,7 @@
 import discord, aiohttp, asyncio, socket
 from discord.ext import commands
 from Websocket.ws import Websocket
+from Websocket.websocket import WebSocket
 from database import db
 
 class MimirQuiz(commands.Cog, Websocket):
@@ -73,55 +74,114 @@ class MimirQuiz(commands.Cog, Websocket):
         if not web_url: return await ctx.reply(ctx.author.mention + ", Channel not setup for Mimir Quiz.")
         await ws.get_quiz_details(get_type = "send", game_num = game_num)
     
+    @commands.command()
+    async def login(self, ctx, username = None, password = None):
+        """Login to Display."""
+        if "Display Access" not in [role.name for role in ctx.author.roles]:
+            return await ctx.reply(ctx.author.mention + ", You need `Display Access` role to run this command!")
+        if not username or not password:
+            return await ctx.reply(ctx.author.mention + ", You didn't mention username or password.\n```\n{}{} [username] [password]\n```".format(ctx.prefix, ctx.command.name))
+        ws = WebSocket(guild_id = ctx.guild.id, client = self.client)
+        web_url = await ws.get_web_url()
+        if not web_url: return await ctx.reply(ctx.author.mention + ", Channel not setup for Display Trivia.")
+        response = await ws.get_sub_protocol(username, password)
+        if not response: return await ctx.reply(ctx.author.mention + ", Enter username or password is incorrect!")
+        update = {"username": username, "password": password}
+        db.display_details.update_one({"guild_id": ctx.guild.id}, {"$set": update})
+        await ctx.reply(ctx.author.mention + ", You have successfully login to Display!")
+        await ctx.message.delete()
+    
     @commands.command(aliases = ["open"])
     @commands.cooldown(1, 10, commands.BucketType.guild)
-    async def start(self, ctx):
+    async def start(self, ctx, trivia = "mimir"):
         """Start Websocket."""
-        if "Mimir Access" not in [role.name for role in ctx.author.roles]:
-            return await ctx.reply(ctx.author.mention + ", You need `Mimir Access` role to run this command!")
-        ws = Websocket(guild_id = ctx.guild.id, client = self.client)
-        web_url = await ws.get_web_url()
-        if not web_url: return await ctx.reply(ctx.author.mention + ", You didn't setup any channel for Mimir Quiz.")
-        if not ws.is_ws_open:
+        if trivia.lower() == "mimir":
+            if "Mimir Access" not in [role.name for role in ctx.author.roles]:
+                return await ctx.reply(ctx.author.mention + ", You need `Mimir Access` role to run this command!")
+            ws = Websocket(guild_id = ctx.guild.id, client = self.client)
+            web_url = await ws.get_web_url()
+            if not web_url: return await ctx.reply(ctx.author.mention + ", You didn't setup any channel for Mimir Quiz.")
+            if not ws.is_ws_open:
+                await ws.send_hook("**Websocket Opened!**")
+                await ws.start_hook()
+            else:
+                await ws.send_hook("**Websocket Already Opened!**")
+        elif trivia.lower() == "display":
+            if "Display Access" not in [role.name for role in ctx.author.roles]:
+                return await ctx.reply(ctx.author.mention + ", You need `Display Access` role to run this command!")
+            ws = WebSocket(guild_id = ctx.guild.id, client = self.client)
+            web_url = await ws.get_web_url()
+            if not web_url: return await ctx.reply(ctx.author.mention + ", You didn't setup any channel for Display Trivia.")
+            await ws.get_ws()
+            if ws.ws:
+                if ws.ws.open:
+                    return await ws.send_hook("**Websocket Already Opened!**")
             await ws.send_hook("**Websocket Opened!**")
-            await ws.start_hook()
+            await ws.connect_ws()
         else:
-            await ws.send_hook("**Websocket Already Opened!**")
+            await ctx.reply(ctx.author.mention + ', Please mention between `Display` or `Mimir`!')
          
     @commands.command()
     @commands.cooldown(1, 10, commands.BucketType.guild)
-    async def close (self, ctx):
+    async def close (self, ctx, trivia = "mimir"):
         """Close Websocket."""
-        if "Mimir Access" not in [role.name for role in ctx.author.roles]:
-            return await ctx.reply(ctx.author.mention + ", You need `Mimir Access` role to run this command!")
-        ws = Websocket(guild_id = ctx.guild.id, client = self.client)
-        web_url = await ws.get_web_url()
-        if not web_url: return await ctx.reply(ctx.author.mention + ", You didn't setup any channel for Mimir Quiz.")
-        if ws.is_ws_open:
-            await ws.close_hook()
+        if trivia.lower() == "mimir":
+            if "Mimir Access" not in [role.name for role in ctx.author.roles]:
+                return await ctx.reply(ctx.author.mention + ", You need `Mimir Access` role to run this command!")
+            ws = Websocket(guild_id = ctx.guild.id, client = self.client)
+            web_url = await ws.get_web_url()
+            if not web_url: return await ctx.reply(ctx.author.mention + ", You didn't setup any channel for Mimir Quiz.")
+            if ws.is_ws_open:
+                await ws.close_hook()
+            else:
+                await ws.send_hook("**Websocket Already Closed!**")
+        elif trivia.lower() == "display":
+            if "Display Access" not in [role.name for role in ctx.author.roles]:
+                return await ctx.reply(ctx.author.mention + ", You need `Display Access` role to run this command!")
+            ws = WebSocket(guild_id = ctx.guild.id, client = self.client)
+            web_url = await ws.get_web_url()
+            if not web_url: return await ctx.reply(ctx.author.mention + ", You didn't setup any channel for Display Trivia.")
+            await ws.close_ws()
         else:
-            await ws.send_hook("**Websocket Already Closed!**")
+            await ctx.reply(ctx.author.mention + ', Please mention between `Display` or `Mimir`!')
         
     @commands.command()
     @commands.cooldown(1, 10, commands.BucketType.guild)
-    async def setup(self, ctx, channel: discord.TextChannel = None):
+    async def setup(self, ctx, trivia = "mimir", channel: discord.TextChannel = None):
         """Setup mimir quiz channel."""
         if not ctx.author.guild_permissions.administrator:
             return await ctx.reply(ctx.author.mention + ", You don't have enough permission to run this command!")
         if not channel: channel = ctx.channel
-        webhook = await channel.create_webhook(name = "Mimir Quiz")
-        check = db.mimir_details.find_one({"guild_id": ctx.guild.id})
-        if check:
-            update = {"web_url": webhook.url}
-            db.mimir_details.update_one({"guild_id": ctx.guild.id}, {"$set": update})
-            embed = discord.Embed(title = "Mimir Quiz Channel Updated!", color = discord.Colour.random())
-            await webhook.send(embed = embed)
-            await ctx.reply(ctx.author.mention + ", You have successfully setup Mimir Quiz Channel.")
+        if trivia.lower() == "mimir":
+            webhook = await channel.create_webhook(name = "Mimir Quiz")
+            check = db.mimir_details.find_one({"guild_id": ctx.guild.id})
+            if check:
+                update = {"web_url": webhook.url}
+                db.mimir_details.update_one({"guild_id": ctx.guild.id}, {"$set": update})
+                embed = discord.Embed(title = "Mimir Quiz Channel Updated!", color = discord.Colour.random())
+                await webhook.send(embed = embed)
+                await ctx.reply(ctx.author.mention + ", You have successfully setup Mimir Quiz Channel.")
+            else:
+                db.mimir_details.insert_one({"guild_id": ctx.guild.id, "web_url": webhook.url, "token": "eyJraWQiOiJYS1wvRlBZOFlcL1lJejV1VHJSSEdhOTZ6VHp0M1lWTlwvR25UQ0JrMitlbXNNPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiI1ZTliNTY5YS0wNDIzLTQ4ODMtOTAyZS05YTQxYjM4YjJhYzQiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiaXNzIjoiaHR0cHM6XC9cL2NvZ25pdG8taWRwLnVzLWVhc3QtMS5hbWF6b25hd3MuY29tXC91cy1lYXN0LTFfME93Q2RwclNoIiwiY29nbml0bzp1c2VybmFtZSI6ImJlcm5mcmllZDMyNTBtZXllckBnbWFpbC5jb20iLCJvcmlnaW5fanRpIjoiMGVkYjIyZjQtNDBmMC00MjUwLWI0NzMtMzE3MzUwMzRiNWNkIiwiYXVkIjoiMnN1MG1qY3JqcXN2amlwZTYyYThnY281NDYiLCJldmVudF9pZCI6ImI2MDQ2OTcyLWFjMDYtNDJkYS04MzYyLTYzYWM2ZjNkNWFiMCIsInRva2VuX3VzZSI6ImlkIiwiYXV0aF90aW1lIjoxNjUwMDQzNzkyLCJleHAiOjE2NTAwNDczOTIsImlhdCI6MTY1MDA0Mzc5MiwianRpIjoiMTFjMDFhMTgtYTQyMS00MjVmLTk4N2ItNGY5ZDliZGJjZDk5IiwiZW1haWwiOiJiZXJuZnJpZWQzMjUwbWV5ZXJAZ21haWwuY29tIn0.P6DjqvX_BqT60wliNRFg38RjE7AIU4yyCYv3eh8ShmlN7jO5THvuQp8h5hfa7Oss49xNytPZE7Dk74tEcNgI7QLTKI5_-xDZUGLCvGDenfCtiAK6TKYMgzOWjOVzS3LiCXNLAVjgKTBmq5iOehQPz1XN7Wvxq7cn7xMgnD1XXtJ4i5-jjI4DSpC6gnDSIEUfqpfm3pGv14R_HrIM5-6arWaZuxyPWBywh7fDmh1F5Z47dBd9gDFNR2LKFM1fFcn8-EVtl83FazRUJUO1l_KIio8Z1Awki0O-KvAfX0xGY7HkrFqZd_CdJY93eKHWR0qZZj-fdcq1BVtqyPsp76VlFQ:0xFCCd91Ca80bbf04da2Af2AA9E5569fdA28843D2E"})
+                embed = discord.Embed(title = "Mimir Quiz Channel Updated!", color = discord.Colour.random())
+                await webhook.send(embed = embed)
+                await ctx.reply(ctx.author.mention + ", You have successfully setup Mimir Quiz Channel.")
+        elif trivia.lower() == "display":
+            webhook = await channel.create_webhook(name = "Display Trivia")
+            check = db.display_details.find_one({"guild_id": ctx.guild.id})
+            if check:
+                update = {"web_url": webhook.url}
+                db.display_details.update_one({"guild_id": ctx.guild.id}, {"$set": update})
+                embed = discord.Embed(title = "Display Trivia Channel Updated!", color = discord.Colour.random())
+                await webhook.send(embed = embed)
+                await ctx.reply(ctx.author.mention + ", You have successfully setup Display Trivia Channel.")
+            else:
+                db.display_details.insert_one({"guild_id": ctx.guild.id, "web_url": webhook.url, "username": None, "password": None,"subscription": False})
+                embed = discord.Embed(title = "Display Trivia Channel Updated!", color = discord.Colour.random())
+                await webhook.send(embed = embed)
+                await ctx.reply(ctx.author.mention + ", You have successfully setup Display Trivia Channel.")
         else:
-            db.mimir_details.insert_one({"guild_id": ctx.guild.id, "web_url": webhook.url, "token": "eyJraWQiOiJYS1wvRlBZOFlcL1lJejV1VHJSSEdhOTZ6VHp0M1lWTlwvR25UQ0JrMitlbXNNPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiI1ZTliNTY5YS0wNDIzLTQ4ODMtOTAyZS05YTQxYjM4YjJhYzQiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiaXNzIjoiaHR0cHM6XC9cL2NvZ25pdG8taWRwLnVzLWVhc3QtMS5hbWF6b25hd3MuY29tXC91cy1lYXN0LTFfME93Q2RwclNoIiwiY29nbml0bzp1c2VybmFtZSI6ImJlcm5mcmllZDMyNTBtZXllckBnbWFpbC5jb20iLCJvcmlnaW5fanRpIjoiMGVkYjIyZjQtNDBmMC00MjUwLWI0NzMtMzE3MzUwMzRiNWNkIiwiYXVkIjoiMnN1MG1qY3JqcXN2amlwZTYyYThnY281NDYiLCJldmVudF9pZCI6ImI2MDQ2OTcyLWFjMDYtNDJkYS04MzYyLTYzYWM2ZjNkNWFiMCIsInRva2VuX3VzZSI6ImlkIiwiYXV0aF90aW1lIjoxNjUwMDQzNzkyLCJleHAiOjE2NTAwNDczOTIsImlhdCI6MTY1MDA0Mzc5MiwianRpIjoiMTFjMDFhMTgtYTQyMS00MjVmLTk4N2ItNGY5ZDliZGJjZDk5IiwiZW1haWwiOiJiZXJuZnJpZWQzMjUwbWV5ZXJAZ21haWwuY29tIn0.P6DjqvX_BqT60wliNRFg38RjE7AIU4yyCYv3eh8ShmlN7jO5THvuQp8h5hfa7Oss49xNytPZE7Dk74tEcNgI7QLTKI5_-xDZUGLCvGDenfCtiAK6TKYMgzOWjOVzS3LiCXNLAVjgKTBmq5iOehQPz1XN7Wvxq7cn7xMgnD1XXtJ4i5-jjI4DSpC6gnDSIEUfqpfm3pGv14R_HrIM5-6arWaZuxyPWBywh7fDmh1F5Z47dBd9gDFNR2LKFM1fFcn8-EVtl83FazRUJUO1l_KIio8Z1Awki0O-KvAfX0xGY7HkrFqZd_CdJY93eKHWR0qZZj-fdcq1BVtqyPsp76VlFQ:0xFCCd91Ca80bbf04da2Af2AA9E5569fdA28843D2E"})
-            embed = discord.Embed(title = "Mimir Quiz Channel Updated!", color = discord.Colour.random())
-            await webhook.send(embed = embed)
-            await ctx.reply(ctx.author.mention + ", You have successfully setup Mimir Quiz Channel.")
+            await ctx.reply(ctx.author.mention + ', Please mention between `Display` or `Mimir`!')
 
 def setup(client):
     client.add_cog(MimirQuiz(client))
