0cce6df9819f099e3c6fb012f941e620d474b481
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2022-05-21 00:15:35 +0530

    update db.py

--- database/db.py
+++ database/db.py
@@ -4,3 +4,4 @@ data = MongoClient('mongodb+srv://Subrata2001:Subrata2001@cluster0.ywnwn.mongodb
 db = data.get_database("MimirQuiz")
 mimir_details = db.mimir_details
 display_details = db.display_details
+hq_details = db.hq_details
