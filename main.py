6bcf67accb1a49180b74a1c23bec79c0ae59073e
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2022-05-21 23:38:28 +0530

    update main.py

--- main.py
+++ main.py
@@ -96,7 +96,7 @@ class MainClass(commands.Cog):
             return await ctx.author.send(ctx.author.mention + ", You failed to send your order ID within time. Don't worry if already paid the amount then start this session again and send your ID.")
         id = message.content.strip()
         if len(id) != 18:
-        	if "@" not in id:
+            if "@" not in id:
                 embed = discord.Embed(title = "__Invalid ID !__",
                     description = "Invalid Order ID or Email ID provided! Don't worry if you already paid, start this process again and send your correct ID!")
                 return await ctx.author.send(embed = embed)
