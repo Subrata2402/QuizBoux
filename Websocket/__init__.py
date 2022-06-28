6ed04a60d3323dd61e95f335f2775f2bc1cf7f74
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2022-06-29 02:44:52 +0530

    Update __init__.py

--- Websocket/__init__.py
+++ Websocket/__init__.py
@@ -1,4 +1,4 @@
 from .mimir_ws import MimirWebSocket
 from .display_ws import DisplayWebSocket
 from .hq_ws import HQWebSocket
-from .swagbucks_ws import SbWebSocket
+from .swagbucks_ws import SbWebSocket, SwagbucksLive
