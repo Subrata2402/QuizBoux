735cee0c30f21dfeb0bd9209c4bd501142f8565d
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2022-07-07 23:29:24 +0530

    update swagbucks.py

--- Trivia/swagbucks.py
+++ Trivia/swagbucks.py
@@ -11,6 +11,9 @@ class SwagbucksTrivia(commands.Cog, SwagbucksLive):
 
 	@commands.command()
 	async def sbstart(self, ctx, username: str = None):
+		"""
+		Check and open a websocket by username.
+		"""
 		if not username:
 			return await ctx.send("Username is required.")
 		ws = SbWebSocket(self.client, username.lower())
@@ -23,17 +26,27 @@ class SwagbucksTrivia(commands.Cog, SwagbucksLive):
 		
 	@commands.command()
 	async def sbclose(self, ctx, username: str = None):
+		"""
+		Close a websocket by username.
+		"""
 		ws = SbWebSocket(self.client, username.lower())
 		await ws.close_ws()
 	
 	@commands.command()
 	async def sblogin(self, ctx, email_id: str = None, password: str = None):
+		"""
+		Login a Swagbucks account and stored some required details in the database.
+		"""
 		if not email_id or not password:
 			return await ctx.send("Username or Password is required to login to Swagbucks.")
 		await self.login(email_id, password)
 		
 	@commands.command()
 	async def sbupdate(self, ctx, username: str = None):
+		"""
+		If account is expire then this command will delete the stored account details
+		and login again to update account.
+		"""
 		if not username:
 			return await ctx.send("Required username to update of Swagbucks account.")
 		details = db.sb_details.find_one({"username": username.lower()})
@@ -45,12 +58,33 @@ class SwagbucksTrivia(commands.Cog, SwagbucksLive):
 		
 	@commands.command()
 	async def details(self, ctx, username: str = None):
+		"""
+		Get stats details of a Swagbucks account.
+		"""
 		if not username:
 			return await ctx.send("Required username to get details of Swagbucks account.")
 		await self.account_details(username.lower())
 		
+	
+	@commands.command()
+	async def accounts(self, ctx):
+		"""
+		Get all accounts username, stored in the database.
+		"""
+		accounts = list(db.sb_details.find())
+		description = ""
+		for index, data in enumerate(accounts):
+			description += "{} - {}\n".format(index+1, data["username"])
+		if not accounts:
+			return await ctx.send("No accounts found.")
+		await ctx.send("```\n{}\n```".format(description))
+		
+		
 	@commands.command()
 	async def nextshow(self, ctx):
+		"""
+		Get Swagbucks Live next show details.
+		"""
 		username = list(db.sb_details.find())[0]["username"]
 		ws = SwagbucksLive(self.client, username)
 		await ws.show_details()
