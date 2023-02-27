4dfa85a40aebb105f89f24db25630601a07129e6
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2023-02-27 17:39:46 +0530

    Update quizophy.py

--- Trivia/quizophy.py
+++ Trivia/quizophy.py
@@ -4,6 +4,31 @@ from bs4 import BeautifulSoup
 import random, openai
 import config
 
+google_question = "https://google.com/search?q="
+
+order = ["１", "２", "３", "４", "５", "６", "７", "８", "９", "０"]
+
+ignore_options = ["the", "of", "in", "&", "on", "for", "or", "it", "to", "at", "and", "?", "#", "!",
+"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+"name", "does", "do", "is", "am", "are", "have", "has", "been", "did", "was", "not", "least", "never", "don't", "haven't", "didn't", "wasn't", "except", "wouldn't", "itsn't",
+"were", "had", "will", "would", "shall", "can", "should", "could", "may", "might", "need", "come", "comes", "means",
+"what", "who", "which", "whom", "why", "how", "when", "where", "=", "de",
+"of", "on", "these", "that", "this", "those", "there", "their", "at", "between", "from", "since", "for",
+"they", "and", "the", "a", "an", "with", "as", "by", "in", "to", "into", "also", "but",
+"i", "my", "me", "we", "our", "you", "your", "he", "his", "him", "himself", "them", "themselves", "it", "its", "myself", "she", "her", "yourselves",
+"&", ".", "?", ",", "matched", "paired", "pair", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"
+]
+
+replace_options = {"1": "one", "2": "two", "3": "three", "4": "four", "5": "five",
+"6": "six", "7": "seven", "8": "eight", "9": "nine", "10": "ten"}
+
+negative_words = {
+    " not ", "least", "except", "never", "incorrect", "incorrectly", "none", "cannot", "can’t", "didn’t", "doesn’t", "don’t",
+    "won’t", "hasn’t", "haven’t", "shouldn’t", "wouldn’t", "wasn’t", "aren’t", "isn’t", "couldn’t", "weren’t", "can't", "didn't",
+    "doesn't", "don't", "won't", "hasn't", "haven't", "shouldn't", "wouldn't", "wasn't", "aren't", "isn't", "couldn't", "weren't",
+    "hadn't", "hadn’t", "mustn't", "mustn’t"
+}
+
 class Quizophy(commands.Cog):
 	
 	def __init__(self, client):
