5f365718354a4823bdcba29b1878227986055eba
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2023-02-27 13:53:15 +0530

    Update quizophy.py

--- Trivia/quizophy.py
+++ Trivia/quizophy.py
@@ -2,31 +2,7 @@ import requests, discord, re
 from discord.ext import commands
 from bs4 import BeautifulSoup
 import random, openai
-
-google_question = "https://google.com/search?q="
-
-order = ["１", "２", "３", "４", "５", "６", "７", "８", "９", "０"]
-
-ignore_options = ["the", "of", "in", "&", "on", "for", "or", "it", "to", "at", "and", "?", "#", "!",
-"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
-"name", "does", "do", "is", "am", "are", "have", "has", "been", "did", "was", "not", "least", "never", "don't", "haven't", "didn't", "wasn't", "except", "wouldn't", "itsn't",
-"were", "had", "will", "would", "shall", "can", "should", "could", "may", "might", "need", "come", "comes", "means",
-"what", "who", "which", "whom", "why", "how", "when", "where", "=", "de",
-"of", "on", "these", "that", "this", "those", "there", "their", "at", "between", "from", "since", "for",
-"they", "and", "the", "a", "an", "with", "as", "by", "in", "to", "into", "also", "but",
-"i", "my", "me", "we", "our", "you", "your", "he", "his", "him", "himself", "them", "themselves", "it", "its", "myself", "she", "her", "yourselves",
-"&", ".", "?", ",", "matched", "paired", "pair", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"
-]
-
-replace_options = {"1": "one", "2": "two", "3": "three", "4": "four", "5": "five",
-"6": "six", "7": "seven", "8": "eight", "9": "nine", "10": "ten"}
-
-negative_words = {
-    " not ", "least", "except", "never", "incorrect", "incorrectly", "none", "cannot", "can’t", "didn’t", "doesn’t", "don’t",
-    "won’t", "hasn’t", "haven’t", "shouldn’t", "wouldn’t", "wasn’t", "aren’t", "isn’t", "couldn’t", "weren’t", "can't", "didn't",
-    "doesn't", "don't", "won't", "hasn't", "haven't", "shouldn't", "wouldn't", "wasn't", "aren't", "isn't", "couldn't", "weren't",
-    "hadn't", "hadn’t", "mustn't", "mustn’t"
-}
+import config
 
 class Quizophy(commands.Cog):
 	
@@ -268,8 +244,5 @@ class Quizophy(commands.Cog):
 		embed.description = f"```\n{res}\n```"
 		await ctx.send(embed = embed)
 		
-client = commands.Bot(command_prefix = ">", description="This is a trivia bot for discord server.")
-client.add_cog(Quizophy(client))
-
-
-client.run("ODU4MzcxMDk2MjIyNzYxMDAw.G7iOn5.3cIXRr9zhtl2KemUSlqoInXVFO03JA4-lrssgU")
+def setup(client):
+	client.add_cog(Quizophy(client))
